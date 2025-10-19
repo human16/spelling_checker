4b4aec4bf492f277fc51cdf4dd83291b80947a7b
ben <by240@scarletmail.rutgers.edu>
2025-10-19 11:49:42 -0400

    created read_inputs and add README

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 netid: by240, elp95
+
+# Testing
+
+
+# Planning
+
+## Reading files/directories
+
+To read the file names, the directory to the dictionary and the suffix of files, we will first make a few assumptions
+
+* There's only one or no occurance of `-s`
+* `-s` must occur as the first argument
+* without `-s`, `spell` will assume the suffix `.txt`
+* If `-s` occurs, the next argument will be the suffix, following the format: `.???` for whatever the suffix is for scanning files
+* The next argument after that must be the name of the dictionary file
+* The rest of the argument will be either files or directories
+* Files will not have the suffix included in the argument
+* Directories will be serated with `/`
--- checker.c
+++ checker.c
@@ -1,5 +1,52 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
 
-int main(int argc, char **argv) {
+#ifndef DEBUG
+#define DEBUG 1
+#endif
+
+int read_inputs(int argc, char *argv[], char *suffix, char **files, char **directories) {
+    if (argc < 3) {
+        printf("Missing arguments");
+        return 1;
+    }
+    //making sure the suffix exists
+    int suffix_arg_exists = 0;
+    if (!strcmp(argv[1], "-s")) {
+        suffix_arg_exists = 1;
+        if (*argv[2] != '.') {
+            printf("Incorrect suffix format");
+            return 1;
+        }
+        suffix = argv[2];
+    }
+
+    //starting after the index after the suffix if it exists 
+    for (int i = 1 + 2 * (suffix_arg_exists); i < argc; i++) {
+        if (!strcmp(argv[i], "-s")) {
+            printf("-s appears in a place that's not the first position");
+        }
+        
+    }
+
+    return 0;
+}
+
+
+int main(int argc, char *argv[]) {
+    char *suffix = ".txt";
+    char **files = malloc((argc - 1)*sizeof(char *)); // assuming all arguments are files
+    char **directories = malloc((argc - 1)*sizeof(char *)); // assuming all arguments are directories
+    int result = read_inputs(argc, argv, suffix, files, directories);
+    if (result != 0) {
+        free(files);
+        free(suffix);
+        free(directories);
+        return 1;
+    }
+
+
     return 0;
 }
