2d79b59b79251a851a25db037ca75080a8db546e
Edson Petry <edsonlbpetry@gmail.com>
2025-10-24 16:08:33 -0400

    feat(trie): define trie header file

---
+++ trie.h
@@ -0,0 +1,22 @@
+#ifndef TRIE_H
+#define TRIE_H
+
+// include lowercase and uppercase
+// english letters.
+#define ALPHABET_SIZE 52
+
+typedef struct TrieNode TrieNode;
+struct TrieNode {
+  char data;
+  TrieNode* children[ALPHABET_SIZE];
+  int leaf;
+}
+
+TrieNode* make_trienode(char data);
+void free_trienode(TrieNode* node);
+TrieNode* insert_trie(TrieNode* root, char* word);
+int search_trie(TrieNode* root, char* word);
+void print_trie(TrieNode* root);
+void print_search(TrieNode* root, char* word);
+
+#endif
